170f618b66651ac5d11f7c0adae6df52afa2bded
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-01-19 09:47:03 +0000

    1) Выставил корректный dpi = 72 2) Исправил функцию преобразования координат.

--- datasets/ICDAR/ICDAR.py
+++ datasets/ICDAR/ICDAR.py
@@ -62,49 +62,41 @@ class Icdar(tfds.core.GeneratorBasedBuilder):
       region_file_path = pdf_file_path.with_name(stem + '-reg.xml')
       structure_file_path = pdf_file_path.with_name(stem + '-str.xml')
 
-      pages = pdf2image.convert_from_path(pdf_file_path, dpi=68)
-      for page_number, table in self._generate_tables(region_file_path, structure_file_path):
-        key = '{}-{}'.format(stem, page_number)
+      pages = pdf2image.convert_from_path(pdf_file_path, dpi=72)
+      for page_number, table in self._generate_tables(pages[0].height, region_file_path, structure_file_path):
+        key = '{}-{}'.format(stem, table.id)
         page = pages[page_number]
-        page_height = page.height
-        dx = -5
-        dy = 25
-        crop_rect = (
-          table.rect.left + dx, page_height - table.rect.bottom + dy, 
-          table.rect.right + dx, page_height - table.rect.top + dy
-          )
-        table_image = page.crop(crop_rect)
-        #table_image = page.crop(table.rect)
-        page.save('page.png')
-        table_image.save('table.png')
+        table_image = page.crop(table.rect)
+        # Uncomment to debug
+        # table_image.save('table-{}.png'.format(table.id))
         yield key, {
           'image': self._image_to_byte_array(table_image),
           'horz_split_points': self._image_to_byte_array(table.create_horz_split_points_image()),
           'vert_split_points': self._image_to_byte_array(table.create_vert_split_points_image())
         }
 
-  def _generate_tables(self, region_file_path, structure_file_path):
+  def _generate_tables(self, page_height, region_file_path, structure_file_path):
     regions_tree = ET.parse(region_file_path)
     structures_tree = ET.parse(structure_file_path)
     for table_node, table_structure_node in zip(regions_tree.getroot(), structures_tree.getroot()):
+      table_id = int(table_node.get('id'))
       region_node = table_node.find('region')
       page_number = int(region_node.get('page')) - 1
-      table_rect = self._get_bounding_box(region_node)
+      table_rect = self._get_bounding_box(page_height, region_node)
       cells_node = table_structure_node.find('region')
-      cells = [self._get_cell(node) for node in cells_node]
-      yield page_number, Table(table_rect, cells)
+      cells = [self._get_cell(page_height, node) for node in cells_node]
+      yield page_number, Table(table_id, table_rect, cells)
 
-  def _get_bounding_box(self, xml_node):
+  def _get_bounding_box(self, page_height, xml_node):
     bounding_box_node = xml_node.find('bounding-box')
-    return Rect(
-      int(bounding_box_node.get('x1')),
-      int(bounding_box_node.get('y1')),
-      int(bounding_box_node.get('x2')),
-      int(bounding_box_node.get('y2'))
-    )
+    left = int(bounding_box_node.get('x1'))
+    top = page_height - int(bounding_box_node.get('y2'))
+    right = int(bounding_box_node.get('x2'))
+    bottom = page_height - int(bounding_box_node.get('y1'))
+    return Rect(left, top, right, bottom)
 
-  def _get_cell(self, xml_node):
-    rect = self._get_bounding_box(xml_node)
+  def _get_cell(self, page_height, xml_node):
+    rect = self._get_bounding_box(page_height, xml_node)
     col_start = int(xml_node.get('start-col'))
     col_end = int(xml_node.get('end-col', col_start))
     row_start = int(xml_node.get('start-row'))
@@ -131,7 +123,8 @@ class Cell(object):
 
 
 class Table(object):
-  def __init__(self, rect, cells):
+  def __init__(self, id, rect, cells):
+    self.id = id
     self.rect = rect
     self.cells = cells
 

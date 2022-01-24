a2af96bb4098f1c6ef514db2b527dd832a1c2c1c
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-01-24 15:27:50 +0000

    Добавил тест на выходной размер модели.

---
+++ split/test_model.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+import tensorflow as tf
+from model import Model
+
+class ModelTestCase(TestCase):
+    def test_output_shape(self):
+        batch_size = 1
+        height = 200
+        width = 1000
+        random_image = tf.random.uniform(shape=(batch_size, height, width, 3), minval=0, maxval=256, dtype='float32', seed=42)
+        m = Model()
+        (horz_split_points_probs1, horz_split_points_probs2, horz_split_points_probs3,
+            vert_split_points_probs1, vert_split_points_probs2, vert_split_points_probs3) = m(random_image)
+
+        self.assertEqual(horz_split_points_probs1.shape, (batch_size, height))
+        self.assertEqual(horz_split_points_probs2.shape, (batch_size, height))
+        self.assertEqual(horz_split_points_probs3.shape, (batch_size, height,))
+        self.assertEqual(vert_split_points_probs1.shape, (batch_size, width))
+        self.assertEqual(vert_split_points_probs2.shape, (batch_size, width))
+        self.assertEqual(vert_split_points_probs3.shape, (batch_size, width))
+
+if __name__ == '__main__':
+    main()

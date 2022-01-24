f8b7aa7c52b01eb3cefc55754edb77a5a0a8143c
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-01-24 11:01:40 +0000

    Ускорил тест test_large_input до 190мс.

--- split/binarize_layer.py
+++ split/binarize_layer.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 import igraph
 
-def get_integer_capacity(value):
-    return int(1024*value)
+def get_capacity(value):
+    return int(1024 * value)
 
 class BinarizeLayer(keras.layers.Layer):
     """Binarize input probabilities via graph-cut algorithm."""
@@ -12,24 +13,35 @@ class BinarizeLayer(keras.layers.Layer):
         assert gc_lambda >= 0
         self.gc_lambda = gc_lambda
 
-    def call(self, inputs):
-        graph, source, sink = self._create_graph(inputs)
+    def call(self, probs):
+        probs = probs.numpy()
+        graph, source, sink = self._create_graph(probs)
         reachable_nodes, _ = graph.st_mincut(source, sink, 'capacity')
         reachable_nodes.remove(source)
-        result = [0] * len(inputs)
+        result = [0] * len(probs)
         for node in reachable_nodes:
             result[node-1] = 1
         return tf.constant(result, dtype=tf.int32)
 
-    def _create_graph(self, inputs):
+    def _create_graph(self, probs):
+        n = len(probs)
+        assert n > 0
         result = igraph.Graph()
         source = 0
-        sink = len(inputs) + 1
-        result.add_vertices(len(inputs) + 2)
+        sink = n + 1
+        result.add_vertices(n + 2)
 
-        for i, prob in enumerate(inputs):
-            result.add_edges([(source, i+1)], {'capacity': [get_integer_capacity(prob)]})
-            result.add_edges([(i+1, sink)], {'capacity': [get_integer_capacity(1-prob)]})
-            if i > 0:
-                result.add_edges([(i, i+1)], {'capacity': [get_integer_capacity(self.gc_lambda)]})
+        result.add_edges( 
+            [(source, i+1) for i in range(n)], 
+            {'capacity': [get_capacity(p) for p in probs]} 
+        )
+        result.add_edges( 
+            [(i+1, sink) for i in range(n)], 
+            {'capacity': [get_capacity(1-p) for p in probs]} 
+        )
+        if n > 1:
+            result.add_edges(
+                [(i+1, i+2) for i in range(n-1)],
+                {'capacity': [get_capacity(self.gc_lambda)] * (n-1)}
+            )
         return result, source, sink

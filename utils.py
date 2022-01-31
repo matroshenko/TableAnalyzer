bcf3e0f0df09cec7d8d22249118f18752852b83e
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-01-31 19:25:14 +0000

    Вынес методы отрисовки изображений в отдельный файл.

--- utils.py
+++ utils.py
@@ -1,10 +1,3 @@
-from PIL import Image, ImageDraw
-
-from datasets.ICDAR.rect import Rect
-from datasets.ICDAR.grid_structure import GridStructureBuilder
-from datasets.ICDAR.cells_structure import CellsStructureBuilder
-
-
 class Interval(object):
     def __init__(self, start, end):
         assert start < end
@@ -40,35 +33,3 @@ def get_intervals_of_ones(mask):
         assert current_inteval_start is not None
         result.append(Interval(current_inteval_start, len(mask)))
     return result
-
-def create_split_result_image(table_image, horz_split_points_mask, vert_split_points_mask):
-    height = len(horz_split_points_mask)
-    width = len(vert_split_points_mask)
-    assert table_image.size == (width, height)
-    split_points_image = table_image.copy()
-    draw = ImageDraw.Draw(split_points_image)
-
-    for interval in get_intervals_of_ones(horz_split_points_mask):
-        draw.rectangle((0, interval.start, width, interval.end), fill=(255, 0, 0))
-
-    for interval in get_intervals_of_ones(vert_split_points_mask):
-        draw.rectangle((interval.start, 0, interval.end, height), fill=(255, 0, 0))
-    
-    return Image.blend(table_image, split_points_image, 0.5)
-
-def create_merge_result_image(
-        table_image, horz_split_points_mask, vert_split_points_mask,
-        merge_right_mask, merge_down_mask):
-    height = len(horz_split_points_mask)
-    width = len(vert_split_points_mask)
-    assert table_image.size == (width, height)
-    grid_structure = GridStructureBuilder(
-        Rect(0, 0, width, height), horz_split_points_mask, vert_split_points_mask).build()
-    cells_structure = CellsStructureBuilder(merge_right_mask, merge_down_mask).build()
-    
-    result_image = table_image.copy()
-    draw = ImageDraw.Draw(result_image)
-    for cell in cells_structure:
-        rect = grid_structure.get_cell_rect(cell)
-        draw.rectangle(rect.as_tuple(), outline=(255, 0, 0))
-    return result_image
---
+++ visualization.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageDraw
+
+from datasets.ICDAR.rect import Rect
+from datasets.ICDAR.grid_structure import GridStructureBuilder
+from datasets.ICDAR.cells_structure import CellsStructureBuilder
+from utils import get_intervals_of_ones
+
+
+def create_split_result_image(table_image, horz_split_points_mask, vert_split_points_mask):
+    height = len(horz_split_points_mask)
+    width = len(vert_split_points_mask)
+    assert table_image.size == (width, height)
+    split_points_image = table_image.copy()
+    draw = ImageDraw.Draw(split_points_image)
+
+    for interval in get_intervals_of_ones(horz_split_points_mask):
+        draw.rectangle((0, interval.start, width, interval.end), fill=(255, 0, 0))
+
+    for interval in get_intervals_of_ones(vert_split_points_mask):
+        draw.rectangle((interval.start, 0, interval.end, height), fill=(255, 0, 0))
+    
+    return Image.blend(table_image, split_points_image, 0.5)
+
+def create_merge_result_image(
+        table_image, horz_split_points_mask, vert_split_points_mask,
+        merge_right_mask, merge_down_mask):
+    height = len(horz_split_points_mask)
+    width = len(vert_split_points_mask)
+    assert table_image.size == (width, height)
+    grid_structure = GridStructureBuilder(
+        Rect(0, 0, width, height), horz_split_points_mask, vert_split_points_mask).build()
+    cells_structure = CellsStructureBuilder(merge_right_mask, merge_down_mask).build()
+    
+    result_image = table_image.copy()
+    draw = ImageDraw.Draw(result_image)
+    for cell in cells_structure:
+        rect = grid_structure.get_cell_rect(cell)
+        draw.rectangle(rect.as_tuple(), outline=(255, 0, 0))
+    return result_image

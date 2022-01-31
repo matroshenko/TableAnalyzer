17bd5abd898c066959aa5eb125ed8b0a1868d020
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-01-31 19:08:15 +0000

    Добавил функцию отрисовки решетки.

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 from PIL import Image, ImageDraw
 
+from datasets.ICDAR.rect import Rect
+from datasets.ICDAR.grid_structure import GridStructureBuilder
+from datasets.ICDAR.cells_structure import CellsStructureBuilder
+
 
 class Interval(object):
     def __init__(self, start, end):
@@ -37,10 +41,10 @@ def get_intervals_of_ones(mask):
         result.append(Interval(current_inteval_start, len(mask)))
     return result
 
-
 def create_split_result_image(table_image, horz_split_points_mask, vert_split_points_mask):
     height = len(horz_split_points_mask)
     width = len(vert_split_points_mask)
+    assert table_image.size == (width, height)
     split_points_image = table_image.copy()
     draw = ImageDraw.Draw(split_points_image)
 
@@ -51,3 +55,20 @@ def create_split_result_image(table_image, horz_split_points_mask, vert_split_po
         draw.rectangle((interval.start, 0, interval.end, height), fill=(255, 0, 0))
     
     return Image.blend(table_image, split_points_image, 0.5)
+
+def create_merge_result_image(
+        table_image, horz_split_points_mask, vert_split_points_mask,
+        merge_right_mask, merge_down_mask):
+    height = len(horz_split_points_mask)
+    width = len(vert_split_points_mask)
+    assert table_image.size == (width, height)
+    grid_structure = GridStructureBuilder(
+        Rect(0, 0, width, height), horz_split_points_mask, vert_split_points_mask).build()
+    cells_structure = CellsStructureBuilder(merge_right_mask, merge_down_mask).build()
+    
+    result_image = table_image.copy()
+    draw = ImageDraw.Draw(result_image)
+    for cell in cells_structure:
+        rect = grid_structure.get_cell_rect(cell)
+        draw.rectangle(rect.as_tuple(), outline=(255, 0, 0))
+    return result_image

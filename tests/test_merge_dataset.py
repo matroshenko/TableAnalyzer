b6db05c884c44b129ea54cdd3b673c2abc3bbc80
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-01-31 11:04:51 +0000

    Добавил тест для датасета IcdarMerge.

---
+++ tests/test_merge_dataset.py
@@ -0,0 +1,28 @@
+"""ICDAR dataset."""
+
+from functools import partial
+import os
+
+import tensorflow_datasets as tfds
+
+import context
+from datasets.ICDAR.ICDAR import IcdarMerge
+
+
+class IcdarSplitTest(tfds.testing.DatasetBuilderTestCase):
+  """Tests for ICDAR dataset."""
+  DATASET_CLASS = IcdarMerge
+  SPLITS = {
+      'train': 15  # Number of fake train example
+  }
+
+  def _make_builder(self, config=None):
+    return self.DATASET_CLASS(  # pylint: disable=not-callable
+        split_checkpoint_path=os.path.join(self.dummy_data, 'split.ckpt'),
+        data_dir=self.tmp_dir,
+        config=config,
+        version=self.VERSION)
+
+
+if __name__ == '__main__':
+  tfds.testing.test_main()

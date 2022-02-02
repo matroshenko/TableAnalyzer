c112ca1abcae7ea6ca964ad3db1958bf42e7b80a
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-02-02 10:51:35 +0000

    Убрал Flatten слой.

--- merge/model.py
+++ merge/model.py
@@ -41,7 +41,6 @@ class GridPoolingNetworkBlock(keras.layers.Layer):
         self._lower_branch_pool = GridPoolingLayer(True)
         if should_output_predictions:
             self._prediction_layer = GridPoolingLayer(False)
-            self._flatten_layer = keras.layers.Flatten()
 
         self._concat2 = keras.layers.Concatenate()
 
@@ -56,7 +55,6 @@ class GridPoolingNetworkBlock(keras.layers.Layer):
         lower_result = self._lower_branch_conv(middle_result)
         if self._should_output_predictions:
             predictions = self._prediction_layer(lower_result, h_positions, v_positions)
-            predictions = self._flatten_layer(predictions)
         lower_result = self._lower_branch_pool(lower_result, h_positions, v_positions)
 
         result = self._concat2([upper_result, middle_result, lower_result])
@@ -74,7 +72,6 @@ class GridPoolingNetworkFinalBlock(keras.layers.Layer):
         self._concat = keras.layers.Concatenate()
         self._conv1x1 = keras.layers.Conv2D(1, 1, activation='sigmoid')
         self._prediction_layer = GridPoolingLayer(False)
-        self._flatten_layer = keras.layers.Flatten()
 
     def call(self, input, h_positions, v_positions):
         result = self._concat(
@@ -82,7 +79,6 @@ class GridPoolingNetworkFinalBlock(keras.layers.Layer):
         )
         result = self._conv1x1(result)
         result = self._prediction_layer(result, h_positions, v_positions)
-        result = self._flatten_layer(result)
         return result
 
 

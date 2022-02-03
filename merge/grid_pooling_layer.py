47c3b921fe8413347d0a1f58b671d0b407c97222
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-02-03 08:16:45 +0000

    Переписал GridPoolingLayer с использованием tf операций.

--- merge/grid_pooling_layer.py
+++ merge/grid_pooling_layer.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow.keras as keras
 import numpy as np
 
-from datasets.ICDAR.grid_structure import GridStructure, GridStructureBuilder
+from datasets.ICDAR.grid_structure import GridStructureBuilder
 from datasets.ICDAR.rect import Rect
 
 
@@ -14,36 +14,31 @@ class GridPoolingLayer(keras.layers.Layer):
     def call(self, input, h_mask, v_mask):
         assert tf.executing_eagerly()
         assert input.shape[0] == 1
-        input_array = tf.squeeze(input, axis=0).numpy()
+
         h_mask_array = tf.squeeze(h_mask, axis=0).numpy()
         v_mask_array = tf.squeeze(v_mask, axis=0).numpy()
 
-        height = input_array.shape[0]
-        width = input_array.shape[1]
-        channels = input_array.shape[2]
+        height = input.shape[1]
+        width = input.shape[2]
 
         grid = GridStructureBuilder(Rect(0, 0, width, height), h_mask_array, v_mask_array).build()
-        
-        result_shape = (
-            input.shape if self._keep_size 
-            else (1, grid.get_rows_count(), grid.get_cols_count(), channels))
-        result = np.zeros(result_shape, dtype='float32')
 
+        rows = []
         for i in range(grid.get_rows_count()):
+            blocks = []
             for j in range(grid.get_cols_count()):
                 cell = grid.get_cell_rect(Rect(j, i, j+1, i+1))
-                averaged_block = self._get_averaged_block(input_array, cell)
-                if self._keep_size:
-                    result[0, cell.top : cell.bottom, cell.left : cell.right, :] = averaged_block
-                else:
-                    result[0, i, j, :] = averaged_block
-        
-        return tf.convert_to_tensor(result)
+                averaged_block = self._get_averaged_block(input, cell)
+                blocks.append(averaged_block)
+            row = tf.concat(blocks, axis=2)
+            rows.append(row)
+
+        return tf.concat(rows, axis=1)
 
 
     def _get_averaged_block(self, input, cell):
-        block = input[cell.top : cell.bottom, cell.left : cell.right, :]
-        averaged_block = np.mean(block, axis=(0, 1), keepdims=True)
+        block = input[:, cell.top : cell.bottom, cell.left : cell.right, :]
+        averaged_block = tf.reduce_mean(block, axis=(1, 2), keepdims=True)
         if self._keep_size:
             averaged_block = np.broadcast_to(averaged_block, block.shape)
         return averaged_block

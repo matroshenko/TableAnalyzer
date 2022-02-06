0dca2c65e47add6b399c5e65a1d272f5ecb53dfb
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-02-06 15:22:43 +0000

    Оптимизация. Переписал GridPoolingLayer с использованием операций scatter+gather.

--- merge/grid_pooling_layer.py
+++ merge/grid_pooling_layer.py
@@ -20,24 +20,49 @@ class GridPoolingLayer(keras.layers.Layer):
 
         height = input.shape[1]
         width = input.shape[2]
+        channels = input.shape[3]
 
         grid = GridStructureBuilder(Rect(0, 0, width, height), h_mask_array, v_mask_array).build()
 
-        rows = []
+        input = tf.reshape(input, shape=(height*width, channels))
+        multiplier = self._create_multiplier_vector(grid)
+        normalized_input = multiplier * input
+
+        means = tf.zeros(shape=(grid.get_rows_count() * grid.get_cols_count(), channels))
+        indices = self._create_indices(grid)
+        means = tf.tensor_scatter_nd_add(means, np.expand_dims(indices, 1), normalized_input)
+        
+        if not self._keep_size:
+            return tf.reshape(means, shape=(1, grid.get_rows_count(), grid.get_cols_count(), channels))
+
+        result = tf.gather(means, indices)
+        return tf.reshape(result, shape=(1, height, width, channels))
+
+    def _create_multiplier_vector(self, grid):
+        height = grid.get_bounding_rect().get_height()
+        width = grid.get_bounding_rect().get_width()
+
+        result = np.zeros(shape=(height * width, 1), dtype='float32')
         for i in range(grid.get_rows_count()):
-            blocks = []
             for j in range(grid.get_cols_count()):
                 cell = grid.get_cell_rect(Rect(j, i, j+1, i+1))
-                averaged_block = self._get_averaged_block(input, cell)
-                blocks.append(averaged_block)
-            row = tf.concat(blocks, axis=2)
-            rows.append(row)
+                self._update_vector(grid, cell, result, 1 / cell.get_area())
+        return result
 
-        return tf.concat(rows, axis=1)
+    def _update_vector(self, grid, cell, vector, value):
+        width = grid.get_bounding_rect().get_width()
+        for i in range(cell.top, cell.bottom):
+            for j in range(cell.left, cell.right):
+                vector[i * width + j] = value
 
-    def _get_averaged_block(self, input, cell):
-        block = input[:, cell.top : cell.bottom, cell.left : cell.right, :]
-        averaged_block = tf.reduce_mean(block, axis=(1, 2), keepdims=True)
-        if self._keep_size:
-            averaged_block = tf.broadcast_to(averaged_block, block.shape)
-        return averaged_block
+    def _create_indices(self, grid):
+        height = grid.get_bounding_rect().get_height()
+        width = grid.get_bounding_rect().get_width()
+        result = np.zeros(shape=(height * width,), dtype='int32')
+
+        cols_count = grid.get_cols_count()
+        for i in range(grid.get_rows_count()):
+            for j in range(cols_count):
+                cell = grid.get_cell_rect(Rect(j, i, j+1, i+1))
+                self._update_vector(grid, cell, result, i * cols_count + j)
+        return result

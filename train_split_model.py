14dacfb5772b66de7cbf86833e28a952ff3c007b
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-02-08 14:34:44 +0000

    1) Замеряем качество на тесте, он более чистый, чем трейн. 2) Исправил опечатку decay_rate=0.75. 3) На контроле качество замеряем каждые 10 эпох для скорости.

--- train_split_model.py
+++ train_split_model.py
@@ -82,7 +82,7 @@ def main(args):
     lr_schedule = keras.optimizers.schedules.ExponentialDecay(
         args.initial_learning_rate,
         decay_steps=80000,
-        decay_rate=0.075,
+        decay_rate=0.75,
         staircase=True)
 
     # Unfortunately you need to run model in eager mode,
@@ -96,14 +96,14 @@ def main(args):
 
     ds_train, ds_test = tfds.load(
         'icdar_split',
-        split=['train[:90%]', 'train[90%:]']
+        split=['train', 'test']
     )
     ds_train = build_data_pipeline(ds_train, Target.Train, args.max_samples_count)
     ds_test = build_data_pipeline(ds_test, Target.Test, args.max_samples_count)
 
     model.fit(
         ds_train, epochs=args.epochs_count, validation_data=ds_test,
-        callbacks=[get_tensorboard_callback()])
+        callbacks=[get_tensorboard_callback()], validation_freq=10)
     model.save_weights(args.result_file_path, save_format='h5')
 
 if __name__ == '__main__':

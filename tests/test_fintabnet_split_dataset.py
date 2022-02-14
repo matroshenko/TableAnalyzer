bd5c969a2b807b2b2fab6ad6c51b5bb29e3387bc
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-02-14 10:00:06 +0000

    Добавил юнит-тест для FinTabNet.

---
+++ tests/test_fintabnet_split_dataset.py
@@ -0,0 +1,19 @@
+"""ICDAR dataset."""
+
+import tensorflow_datasets as tfds
+
+import context
+from datasets.FinTabNet.FinTabNet import FinTabNetSplit
+
+
+class IcdarSplitTest(tfds.testing.DatasetBuilderTestCase):
+  """Tests for FinTabNet dataset."""
+  DATASET_CLASS = FinTabNetSplit
+  SPLITS = {
+      'val': 16  # Number of fake train example
+  }
+  SKIP_CHECKSUMS = True
+
+
+if __name__ == '__main__':
+  tfds.testing.test_main()

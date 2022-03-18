68d2b3a70de24057e325d3494417dd09676ff50b
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-18 12:26:16 +0000

    Экспортируем модель так, чтобы она могла работать на изображениях произвольного размера.

--- convert_split_to_saved_model.py
+++ convert_split_to_saved_model.py
@@ -4,11 +4,24 @@ import os
 import tensorflow as tf
 
 import split.model
-import split.evaluation
+
+
+class ExportableSplitModel(split.model.Model):
+    def __init__(self):
+        super().__init__(False)
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=(1, None, None, 3), dtype=tf.int32)])
+    def call(self, input):
+        return super().call(input)
 
 
 def main(args):
-    model = split.evaluation.load_model(args.model_file_path, False)
+    # We can't export original model, 
+    # because it won't run on images with dynamic size.
+    model = ExportableSplitModel()
+    model(tf.zeros(shape=(1, 1, 1, 3), dtype=tf.int32))
+    model.load_weights(args.model_file_path)
+
     tf.saved_model.save(model, args.dst_folder_path)
     
 

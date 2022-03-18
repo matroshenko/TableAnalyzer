0728e053a97d37534bde66c756348a83eb1f3f95
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-18 11:55:56 +0000

    Оставил только скрипт для конвертации модели SPLIT.

--- convert_split_to_saved_model.py
+++ convert_split_to_saved_model.py
@@ -1,18 +1,19 @@
 import argparse
+import os
 
+import tensorflow as tf
+
+import split.model
 import split.evaluation
-import merge.evaluation
 
 
 def main(args):
-    module = split.evaluation if args.model_type == 'SPLIT' else merge.evaluation
-    model = module.load_model(args.model_file_path, False)
-    model.save(args.dst_folder_path)
+    model = split.evaluation.load_model(args.model_file_path, False)
+    tf.saved_model.save(model, args.dst_folder_path)
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Converts trained model to SavedModel format.")
-    parser.add_argument('model_type', help='Type of model', choices=['SPLIT', 'MERGE'])
     parser.add_argument('model_file_path', help='Path to trained model checkpoint.')
     parser.add_argument('dst_folder_path', help='Path to folder where saved model will be stored.')
 

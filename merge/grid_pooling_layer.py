6f088fbb9eef62c1c24ffa369946aa73b4bf23ce
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-22 13:15:55 +0000

    Переходим на использование оператора reciprocal_cells_areas_matrix.

--- merge/grid_pooling_layer.py
+++ merge/grid_pooling_layer.py
@@ -5,6 +5,8 @@ import numpy as np
 from table.grid_structure import GridStructure
 from utils.rect import Rect
 
+grid_pooling_helper_ops_module = tf.load_op_library('merge/ops/grid_pooling_helper_ops.so')
+
 
 class GridPoolingLayer(keras.layers.Layer):
     def __init__(self, keep_size):
@@ -19,10 +21,10 @@ class GridPoolingLayer(keras.layers.Layer):
         width = tf.shape(input)[1]
         channels = tf.shape(input)[2]
 
-        multiplier = tf.numpy_function( 
-            self._create_reciprocal_cells_areas_matrix,
-            [height, width, h_positions, v_positions], Tout=tf.float32)
-        normalized_input = multiplier * input
+        multiplier = grid_pooling_helper_ops_module.reciprocal_cells_areas_matrix(
+            height, width, h_positions, v_positions
+        )
+        normalized_input = tf.expand_dims(multiplier, -1) * input
 
         means = tf.zeros(shape=(tf.size(h_positions)+1, tf.size(v_positions)+1, channels))
         indices = tf.numpy_function(
@@ -36,22 +38,6 @@ class GridPoolingLayer(keras.layers.Layer):
         result = tf.gather_nd(means, indices)
         result = tf.ensure_shape(result, shape=input.shape)
         return tf.expand_dims(result, axis=0)
-
-    def _create_reciprocal_cells_areas_matrix(
-            self, height, width, h_positions, v_positions):
-
-        grid = GridStructure(
-            [0] + list(h_positions) + [height], 
-            [0] + list(v_positions) + [width])
-
-        result = np.zeros(shape=(height, width, 1), dtype='float32')
-        for i in range(grid.get_rows_count()):
-            for j in range(grid.get_cols_count()):
-                cell = grid.get_cell_rect(Rect(j, i, j+1, i+1))
-                if cell.is_empty():
-                    continue
-                result[cell.top : cell.bottom, cell.left : cell.right] = 1 / cell.get_area()
-        return result
 
     def _create_indices_matrix(
             self, height, width, h_positions, v_positions):

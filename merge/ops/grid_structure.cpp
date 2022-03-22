8a9d469c98b1d6ad336dff29c17df9f2196be96d
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-22 12:00:12 +0000

    Добавил класс-решетка.

---
+++ merge/ops/grid_structure.cpp
@@ -0,0 +1,51 @@
+#include "grid_structure.h"
+#include <cassert>
+
+/////////////////////////////////////////////////////////////////////
+// Rect
+
+Rect::Rect(int left, int top, int right, int bottom) :
+    Left(left),
+    Top(top),
+    Right(right),
+    Bottom(bottom)
+{
+    assert(Left <= Right && Top <= Bottom);
+}
+
+/////////////////////////////////////////////////////////////////////
+// GridStructure
+
+GridStructure::GridStructure(
+        const vector<int>& _horzPositions, 
+        const vector<int>& _vertPositions) :
+    horzPositions(_horzPositions),
+    vertPositions(_vertPositions)
+{
+    assert(horzPositions.size() >= 2);
+    assert(vertPositions.size() >= 2);
+    assert(isSorted(horzPositions));
+    assert(isSorted(vertPositions));
+}
+
+Rect GridStructure::GetCellRect(int rowIndex, int colIndex) const
+{
+    assert(0 <= rowIndex && rowIndex < GetRowsCount());
+    assert(0 <= colIndex && colIndex < GetColsCount());
+    return Rect(
+        vertPositions[colIndex],
+        horzPositions[rowIndex],
+        vertPositions[colIndex+1],
+        horzPositions[rowIndex+1]
+    );
+}
+
+bool GridStructure::isSorted(const vector<int>& positions) const
+{
+    for(int i = 1; i < positions.size(); ++i) {
+        if(positions[i-1] > positions[i]) {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ merge/ops/grid_structure.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+using std::vector;
+
+struct Rect {
+    int Left;
+    int Top;
+    int Right;
+    int Bottom;
+
+    Rect(int left, int top, int right, int bottom);
+};
+
+
+class GridStructure {
+public:
+    GridStructure(const vector<int>& _horzPositions, const vector<int>& _vertPositions);
+
+    int GetRowsCount() const { return horzPositions.size() - 1; }
+    int GetColsCount() const { return vertPositions.size() - 1; }
+
+    Rect GetCellRect(int rowIndex, int colIndex) const;
+
+private:
+    vector<int> horzPositions;
+    vector<int> vertPositions;
+
+    bool isSorted(const vector<int>& positions) const;
+};

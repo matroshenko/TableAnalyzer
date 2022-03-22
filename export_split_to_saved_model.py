bb1487694bfabbc21ebddc278907865731332c96
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-22 09:15:51 +0000

    1) Упростил использование модели: не нужно добавлять нулевую размерность входному изображению. 2) В результирующей папке содержатся кастомные операции.

--- export_split_to_saved_model.py
+++ export_split_to_saved_model.py
@@ -1,4 +1,5 @@
 import argparse
+import shutil
 import os
 
 import tensorflow as tf
@@ -10,19 +11,27 @@ class ExportableSplitModel(split.model.Model):
     def __init__(self):
         super().__init__(False)
 
-    @tf.function(input_signature=[tf.TensorSpec(shape=(1, None, None, 3), dtype=tf.int32)])
-    def call(self, input):
-        return super().call(input)
+    @tf.function(input_signature=[tf.TensorSpec(shape=(None, None, 3), dtype=tf.int32)])
+    def call(self, image):
+        outputs = super().call(tf.expand_dims(image, 0))
+        return {
+            'horz_split_points_probs': tf.squeeze(outputs['horz_split_points_probs3'], 0),
+            'horz_split_points_binary': tf.squeeze(outputs['horz_split_points_binary'], 0),
+            'vert_split_points_probs': tf.squeeze(outputs['vert_split_points_probs3'], 0),
+            'vert_split_points_binary': tf.squeeze(outputs['vert_split_points_binary'], 0)
+        }
 
 
 def main(args):
-    # We can't export original model, 
-    # because it won't run on images with dynamic size.
     model = ExportableSplitModel()
-    model(tf.zeros(shape=(1, 1, 1, 3), dtype=tf.int32))
+    model(tf.zeros(shape=(1, 1, 3), dtype=tf.int32))
     model.load_weights(args.checkpoint_path)
 
     tf.saved_model.save(model, args.dst_folder_path)
+    # Copy custom ops sources and libs to destination folder.
+    shutil.copytree(
+        './split/ops', os.path.join(args.dst_folder_path, 'ops'), 
+        dirs_exist_ok=True)
     
 
 if __name__ == '__main__':
